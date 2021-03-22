ca04e5822b8bc1f5e049246c5d270c11cd7d0e7d
nimitzufo <nimitzufo@protonmail.com>
2021-03-22 17:07:06 -0400

    the answer my friend is blowing in the wind

--- wordHunting.c
+++ wordHunting.c
@@ -4,78 +4,44 @@
 
 
 int main() {
+	
+	char pesq[MAX];
+	char dictFile[MAX];
+	char textB[MAX];
+	
 
-  const int MAX = 172; //A maior palavra do mundo tem 173 letras
-  char pesq[MAX];
-  printf("Nome do arquivo a ser pesquisado: ");
-  scanf("%s", pesq);
+	
+	FILE * arq;
+	FILE * uarq;
+	FILE * tarq;
+	
+	userMenu(pesq, textB, dictFile);
+	int nlinhas = contalinhas(pesq);
 
-  int nlinhas = contalinhas(pesq);
 
-  FILE * arq;
-  FILE * uarq;
-  FILE * tarq;
-  int i, j, k, tam, tam1, l = 0, contador = 0;
-  char palavras[MAX][MAX];
-  char buffer[nlinhas][MAX];
-  char resul[nlinhas];
-  char dicpalavra[MAX];
-  char ch;
-  int qt = 0;
-  tarq = fopen("meutexto.txt", "w");
-  uarq = fopen(pesq, "r");
-  if (uarq == NULL) {
-    printf("Erro ao abrir o arquivo");
-    return (0);
-  }
-  arq = fopen("dic.txt", "r");
+	tarq = fopen("meutexto.txt", "w");
 
-  while (!feof(arq)) {
-    fgets(dicpalavra, MAX, arq);
-    dicpalavra[strlen(dicpalavra) - 1] = dicpalavra[strlen(dicpalavra)]; //Retira o \0 do fgets
-    tam = strlen(dicpalavra);
-    for (i = 0; i < tam; i++) { //Convertendo palavras do dic em CapsLock
-      dicpalavra[i] = toupper(dicpalavra[i]);
-    }
-    qt = 0;
 
-    //dicpalavra é a palavra a ser pesquisada no texto
 
-    uarq = fopen(pesq, "r"); //Abrindo o arquivo dentro do ciclo, para que ele SEMPRE volte a linha inicial para rodar novamente o texto
-    if (uarq == NULL) {
-      printf("Erro ao abrir o arquivo");
-      return (0);
-    }
+	uarq = openFile(pesq);
+	arq = openFile(dictFile);
+	
+	//resultados da primeira busca
+	huntWords(arq, uarq, pesq);
+	fclose(arq);
+	fclose(tarq);
+		
 
-    while (fscanf(uarq, "%s", palavras[contador]) != EOF && fscanf(uarq, "%c", & ch) != EOF) {
+	uarq = openFile(pesq);
+	arq = openFile(dictFile);
+	//resultados da segunda busca
+	huntWords(arq, uarq, textB);
 
-      tam1 = strlen(palavras[contador]); //RETIRANDO CARACTERES DO FINAL DO TEXTO
-      if (palavras[contador][tam1] == '.' || palavras[contador][tam1] == ',' ||
-        palavras[contador][tam1] == ';' || palavras[contador][tam1] == ':' ||
-        palavras[contador][tam1] == '?' || palavras[contador][tam1] == '-') {
-        palavras[contador][strlen(palavras) - 1] = palavras[contador][strlen(palavras)];
-      } //FIM RETIRANDO CARACTERES DO FINAL DO TEXTO
+	
+	fclose(arq);
+	fclose(tarq);
 
-      for (i = 0; i < tam1; i++) { //Convertendo palavras do dic em CapsLock
-        palavras[contador][i] = toupper(palavras[contador][i]);
-      } //FIM CONVERTENDO PALAVRAS DO DIC EM CAPSLOCK
 
-      if (strcmp(palavras[contador], dicpalavra) == 0) {
-        qt++;
-        printf("A palavra %s está no texto\n", palavras[contador]);
-      } else
-        contador++;
-    }
-    fclose(uarq); //Fechando o arquivo dentro do ciclo, para que ele SEMPRE volte a linha inicial para rodar novamente o texto
 
-    memset(palavras, 0, sizeof(palavras)); //Resetando vetor palavras
-    contador = 0; //Redefinindo contador
 
-    printf("A palavra %s foi encontrada %d no texto\n", dicpalavra, qt);
-    memset(dicpalavra, 0, sizeof(dicpalavra));
-
-  }
-
-  fclose(arq);
-  fclose(tarq);
 }
--- wordHuntingLib.h
+++ wordHuntingLib.h
@@ -2,11 +2,12 @@
 #define BibArquivos_h
 
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-
+#define MAX 172 //A maior palavra do mundo tem 173 letras
 
 int contalinhas(char * pesq);
-
+FILE *openFile(char *pointerFileName);
 
 
 
